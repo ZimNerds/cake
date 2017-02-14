ea144bf9b659f7dfc0c27d5ca01a8eb7323c4835
ZimNerd <eddienyagano@gmail.com>
2017-02-14 09:35:51 +0200

    dropdown 3

--- webroot/js/script.js
+++ webroot/js/script.js
@@ -1,14 +1,17 @@
+$(document).ready(function(){
+
 $('select.country').change(function(){
     $.getJSON(
         '/properties/getstates',
-        'countryid='+$('select.country').val(),
+        'countryid='+$("#country-id").val(),
         function(result){
             $('#states').empty();
             $.each(result.result, function(){
                 $('#states').append('<option>'+this['states']+'</option>');
-                console.log(countryid);
+                console.log($("#country-id").val());
             });
         }
     );
-    console.log(countryid);
+    console.log($("#country-id").val());
+});
 });
