78a56f9ee2777c77fe0ba8cb4b0ef2f47694bc5c
ZimNerd <eddienyagano@gmail.com>
2017-02-14 10:50:19 +0200

    dropdown 7 states 2 oprion 2

--- webroot/js/script.js
+++ webroot/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         var selectedCountry = $(".country option:selected").val();
         $.ajax({
             type: "POST",
-            url: "<?php echo $this->Html->Url(array('action'=>'getstates'));?>",
+            url: "<?php echo $this->Html->Url(array('controller' => 'Properties','action'=>'getstates'));?>",
             data: { country : selectedCountry }
         }).done(function(data){
             $("#response").html(data);
