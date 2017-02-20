9f420ea955a4df48a71bc0b78eecfecc7b91ac1e
ZimNerd <eddienyagano@gmail.com>
2017-02-20 15:34:51 +0200

    Add Auth

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -136,5 +136,6 @@ class UsersController extends AppController
     public function logout()
     {
         return $this->redirect($this->Auth->logout());
+        $this->Flash->success(__('Bye'));
     }
 }
